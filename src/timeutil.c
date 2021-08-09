1d1d46f3e1250999726af3c508d1a2fe2c8adf49
Jeong Arm <kjwonmail@gmail.com>
2021-08-09 17:41:00 +0900

    Fix timeutils

--- src/timeutil.c
+++ src/timeutil.c
@@ -1,22 +1,22 @@
 #include "timeutil.h"
 
 void timeval_diff(const struct timeval* start, const struct timeval* end, struct timeval* result) {
-    if (start->tv_usec < end->tv_usec) {
-        result->tv_usec = start->tv_usec - end->tv_usec + 1000000000;
-        result->tv_sec = start->tv_sec - end->tv_usec - 1;
+    if (end->tv_usec < start->tv_usec) {
+        result->tv_usec = end->tv_usec - start->tv_usec + 1000000000;
+        result->tv_sec = end->tv_sec - start->tv_sec - 1;
     } else {
-        result->tv_usec = start->tv_usec - end->tv_usec;
-        result->tv_sec = start->tv_sec - end->tv_usec;
+        result->tv_usec = end->tv_usec - start->tv_usec;
+        result->tv_sec = end->tv_sec - start->tv_sec;
     }
 }
 
 void timespec_diff(const struct timespec* start, const struct timespec* end, struct timespec* result) {
-    if (start->tv_nsec < end->tv_nsec) {
-        result->tv_nsec = start->tv_nsec - end->tv_nsec + 1000000000;
-        result->tv_sec = start->tv_sec - end->tv_nsec - 1;
+    if (end->tv_nsec < start->tv_nsec) {
+        result->tv_nsec = end->tv_nsec - start->tv_nsec + 1000000000;
+        result->tv_sec = end->tv_sec - start->tv_sec - 1;
     } else {
-        result->tv_nsec = start->tv_nsec - end->tv_nsec;
-        result->tv_sec = start->tv_sec - end->tv_nsec;
+        result->tv_nsec = end->tv_nsec - start->tv_nsec;
+        result->tv_sec = end->tv_sec - start->tv_sec;
     }
 }
 
